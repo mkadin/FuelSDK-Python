6284bcd3dbcee9def183fbfc03c6faf237565c9f
Mike Kadin <kadin@uber.com>
2014-12-10 18:13:28 -0800

    proper version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md') as f:
     readme = f.read()
 
 setup(
-    version='1.0.2UBER',
+    version='99.0.0',
     name='FuelSDK',
     description='ExactTarget Fuel SDK for Python',
     long_description=readme,
